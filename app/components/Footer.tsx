b3b4c2b2e1968a55e1f8db175f3b7cff2789ffe5
James Lawton <jlaw@horizon.io>
2025-03-28 11:53:32 +0000

    added databeat package and footer integration

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,8 +5,15 @@ import {
   TwitterX,
   Youtube,
 } from "react-bootstrap-icons";
+import { Databeat } from '@databeat/tracker'
+
+export const analytics = new Databeat('https://databeat.sequence.app', {
+  jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJzZXEtZG9jcyIsImV4cCI6MTcxMjIxNDYzNn0.qWxTAvdiD1mWUZaEMFwp3_Xq-aFmMzzoIU8h4a8xG9c',
+})
 
 export default function Footer() {
+  analytics.trackView()
+  
   return (
     <footer className="border-t border-white/20 flex justify-center gap-20 py-4">
       <div className="text-14 text-center">
