1287acf5c5a27a4de3fe450991ddd8cbc1d2691f
VGabriel45 <vgabrielmarian21@gmail.com>
2025-05-02 17:10:03 +0300

    fix game inventory

--- app/examples/ContractInventoryWidget/ContractInventoryWidget.tsx
+++ app/examples/ContractInventoryWidget/ContractInventoryWidget.tsx
@@ -2,6 +2,7 @@ import { useOpenWalletModal } from "@0xsequence/wallet-widget";
 import { useOpenConnectModal } from "@0xsequence/connect";
 import { useAccount } from "wagmi";
 import { WalletConnectionDetail } from "~/components/wallet-connection-detail/WalletConnectionDetail";
+import { arbitrumSepolia } from "viem/chains";
 export const ContractInventoryWidget = () => {
   const { address } = useAccount();
   const { setOpenConnectModal } = useOpenConnectModal();
@@ -9,7 +10,21 @@ export const ContractInventoryWidget = () => {
   return address ? (
     <>
       <WalletConnectionDetail address={address} />
-      <button onClick={() => setOpenWalletModal(true)}>
+      <button
+        onClick={() =>
+          setOpenWalletModal(true, {
+            defaultNavigation: {
+              location: "search-collectibles",
+              params: {
+                selectedCollection: {
+                  contractAddress: "0x36631c1e690714192614364ae9629850b546d194",
+                  chainId: arbitrumSepolia.id,
+                },
+              },
+            },
+          })
+        }
+      >
         Open Wallet Inventory
       </button>
     </>
